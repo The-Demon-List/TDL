d3b6d10a9ce23de1f256628562963c3d6f0e5f6b
ZeroLoki500 <riehsb195@gmail.com>
2026-06-24 19:00:20 -0400

    Add route for History component
    Added a route for the History component.

--- js/routes.js
+++ js/routes.js
@@ -3,8 +3,12 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import History from './pages/History.js';
 
-export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
-];
+export default {
+    // ... other configurations if there are any
+    routes: [
+        { path: '/', component: List },
+        { path: '/leaderboard', component: Leaderboard },
+        { path: '/roulette', component: Roulette },
+        { path: '/history', component: History }, // <--- Just add this line right here!
+    ],
+};
