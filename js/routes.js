b6dab8f0f304cef5e9c5afaf4aa2b09ea7c7a54d
ZeroLoki500 <riehsb195@gmail.com>
2026-06-24 18:57:45 -0400

    Add History page import to routes

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import History from './pages/History.js';
 
 export default [
     { path: '/', component: List },
