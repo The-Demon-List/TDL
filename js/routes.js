b879a9d1b53ae4a3d0292d5058547f34b873a517
ZeroLoki500 <riehsb195@gmail.com>
2026-06-24 19:41:11 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,9 +3,16 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import History from './pages/History.js';
 
-export default [
+const routes = [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/history', component: History },
 ];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(),
+    routes,
+});
+
+export default router;
