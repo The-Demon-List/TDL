d590fc010520c7815556a04f1904032cd0e4d4b5
ZeroLoki500 <riehsb195@gmail.com>
2026-06-25 12:21:25 -0400

    Update TimeMachine.js

--- js/pages/TimeMachine.js
+++ js/pages/TimeMachine.js
@@ -1,4 +1,9 @@
-template: `
+import { fetchList } from "../content.js";
+import Spinner from "../components/Spinner.js";
+
+export default {
+    components: { Spinner },
+    template: `
         <main class="page-list">
             <div class="list-container">
                 <h1>Time Machine</h1>
@@ -6,7 +11,7 @@ template: `
                     <label>View list as of: </label>
                     <input type="date" v-model="selectedDate" @change="loadDate">
                 </div>
-                <table class="list" v-if="list.length > 0">
+                <table class="list" v-if="list && list.length > 0">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
                             <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
@@ -17,9 +22,26 @@ template: `
                         </td>
                     </tr>
                 </table>
-                <p v-else style="padding: 20px;">No data found for this date. Check that /data/{{selectedDate}}/ exists.</p>
+                <p v-else style="padding: 20px;">No data found for this date.</p>
             </div>
             <div class="level-container"></div>
             <div class="meta-container"></div>
         </main>
     `,
+    data: () => ({
+        list: [],
+        loading: true,
+        selectedDate: new Date().toISOString().split('T')[0],
+    }),
+    methods: {
+        async loadDate() {
+            this.loading = true;
+            // This expects your fetchList in content.js to handle the date
+            this.list = await fetchList(this.selectedDate);
+            this.loading = false;
+        }
+    },
+    async mounted() {
+        await this.loadDate();
+    }
+};
