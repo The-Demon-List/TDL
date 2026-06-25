9f6511d40cef4a0caa0a4e639d3d7a3b104c20c2
ZeroLoki500 <riehsb195@gmail.com>
2026-06-24 20:19:10 -0400

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,7 @@
+export const store = Vue.reactive({
+    dark: JSON.parse(localStorage.getItem('dark')) || false,
+    toggleDark() {
+        this.dark = !this.dark;
+        localStorage.setItem('dark', JSON.stringify(this.dark));
+    },
+});
