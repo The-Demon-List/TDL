fcd04364d9a02a0d947b59aa56979fb4c66d944d
ZeroLoki500 <riehsb195@gmail.com>
2026-06-24 21:01:45 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -23,7 +23,7 @@ export default {
         <main v-else class="page-list">
             <div class="list-container">
                 <table class="list" v-if="list">
-                    <tr v-for="([level, err], i) in list">
+                    <tr v-for="(item, i) in list">
                         <td class="rank">
                             <p v-if="i + 1 <= 75" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
