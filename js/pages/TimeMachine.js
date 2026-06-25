1c1c100e9892956f49ff6fe3c5c5a4a3be7f1d82
ZeroLoki500 <riehsb195@gmail.com>
2026-06-25 11:52:50 -0400

    Refactor TimeMachine component template
    Refactor TimeMachine component template for clarity and functionality.

--- js/pages/TimeMachine.js
+++ js/pages/TimeMachine.js
@@ -1,11 +1,20 @@
-template: `
-        <main class="page-list">
+import { fetchList } from "../content.js";
+import Spinner from "../components/Spinner.js";
+
+export default {
+    components: { Spinner },
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-list">
+            <h1>Time Machine</h1>
+            <div class="time-machine" style="padding: 20px;">
+                <label>View list as of: </label>
+                <input type="date" v-model="selectedDate" @change="loadDate">
+            </div>
+            
             <div class="list-container">
-                <h1>Time Machine</h1>
-                <div style="padding: 20px;">
-                    <label>View list as of: </label>
-                    <input type="date" v-model="selectedDate" @change="loadDate">
-                </div>
                 <table class="list" v-if="list.length > 0">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
@@ -13,13 +22,28 @@ template: `
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level">
-                            <span class="type-label-lg">{{ level?.name || 'Error' }}</span>
+                            <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
                         </td>
                     </tr>
                 </table>
-            </div>
-            <div class="level-container">
-                <!-- Empty container to satisfy the CSS layout -->
+                <p v-else style="padding: 20px;">No data found for this date.</p>
             </div>
         </main>
     `,
+    data: () => ({
+        list: [],
+        loading: false,
+        selectedDate: new Date().toISOString().split('T')[0],
+    }),
+    methods: {
+        async loadDate() {
+            this.loading = true;
+            // You will need to ensure your content.js/server supports a date query
+            this.list = await fetchList(this.selectedDate); 
+            this.loading = false;
+        }
+    },
+    async mounted() {
+        await this.loadDate();
+    }
+};
