e51babe1a87fd2b441e32b526629927ec922104b
ZeroLoki500 <riehsb195@gmail.com>
2026-06-25 10:47:28 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 import routes from './routes.js';
-import { store } from './store.js';
+
+const app = Vue.createApp({});
+export const store = Vue.reactive({
+    dark: JSON.parse(localStorage.getItem('dark')) || false,
+    toggleDark() {
+        this.dark = !this.dark;
+        localStorage.setItem('dark', JSON.stringify(this.dark));
+    },
+});
 
 const app = Vue.createApp({
     data: () => ({ store }),
 });
-
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes,
 });
 
 app.use(router);
+
 app.mount('#app');
