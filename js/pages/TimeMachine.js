dc526d55a020ea9bf506e2b0a1e9fa6339969a3c
ZeroLoki500 <riehsb195@gmail.com>
2026-06-25 14:58:24 -0400

    Update TimeMachine.js

--- js/pages/TimeMachine.js
+++ js/pages/TimeMachine.js
@@ -2,28 +2,25 @@ import { fetchList } from "../content.js";
 
 export default {
     template: `
-        <main class="page-list">
-            <div class="list-container">
-                <header class="page-header">
-                    <h1>Time Machine</h1>
-                    <div class="date-picker">
-                        <input type="date" v-model="selectedDate" @change="loadDate">
-                    </div>
-                </header>
+  <main class="page-list" style="width: 100%; display: block;">
+    <div class="list-container" style="width: 100%;">
+      <header class="page-header">
+        <h1>Time Machine</h1>
+        <div class="date-picker">
+          <input type="date" v-model="selectedDate" @change="loadDate">
+        </div>
+      </header>
 
-                <table class="list">
-                    <tr v-for="([level, err], i) in list" :key="i">
-                        <td class="rank">
-                            <p class="type-label-lg">#{{ i + 1 }}</p>
-                        </td>
-                        <td class="level">
-                            <span class="type-label-lg">{{ level?.name || 'Error loading level' }}</span>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </main>
-    `,
+      <table class="list" style="width: 100%;">
+        <tr v-for="([level, err], i) in list" :key="i">
+          <td>
+            <span class="type-label-lg">{{ level?.name || 'Error loading level' }}</span>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </main>
+`,
     data: () => ({
         list: [],
         selectedDate: new Date().toISOString().split('T')[0],
